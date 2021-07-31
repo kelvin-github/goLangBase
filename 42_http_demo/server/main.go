b3e27ac314f0c33849fb88e70622f4c49e706fa3
kelvin-github <kelvin.y@126.com>
2021-07-31 11:17:23 +0800

    20210731init

--- 42_http_demo/server/main.go
+++ 42_http_demo/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,21 +9,36 @@ import (
 
 // http server
 func main(){
-    http.HandleFunc("/path01/01/",f1)
+    http.HandleFunc("/",f1)
+    http.HandleFunc("/api",f2)
     http.ListenAndServe("127.0.0.1:9091",nil)
     // http.ListenAndServe("0.0.0.0:9091",nil)  //上服务器时写
 
+}
+
+func f2(w http.ResponseWriter,reve *http.Request){
+    receContent := reve.URL.Query()
+    fmt.Println(receContent) //get 在这里 收取
+
+    // 根据请求body创建一个json解析器实例
+    decoder := json.NewDecoder(reve.Body)   //post 在这里 收取
+    fmt.Println(decoder)
+
+    // 处理逻辑
+
+    // 返回数据
+    // str := `get it xxxx `
+    // w.Write( []byte(str) )
 }
 
 func f1(w http.ResponseWriter,r *http.Request){
     // str := `<h1 style="color:red">hello kelvin</h1>`
     // w.Write( []byte(str) )
 
-    b,err:=ioutil.ReadFile("./templete/test.html")
+    b,err:=ioutil.ReadFile("./templete/index.html")
     if err!=nil{
         fmt.Println("read file fail ,err",err)
         w.Write( []byte( fmt.Sprintf("%v",err) ) )        
     }
     w.Write( b )
-
 }
