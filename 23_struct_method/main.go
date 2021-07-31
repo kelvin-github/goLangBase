1f2bd244174dff3e5059979e6fdfc50aebf11a89
kelvin-github <kelvin.y@126.com>
2021-07-31 12:10:51 +0800

    20210731init

--- 23_struct_method/main.go
+++ 23_struct_method/main.go
@@ -14,11 +14,14 @@ func newDog(name string) dog {
 }
 
 // 方法是作用于 特定类型的 函数 （为这个结构体 定义 一个绑定 的 方法，表行为 ）
+// 接收者表示 调用方法 的 具体类型 变量，多用类型首字母小写 表示
 func (d dog) wang() {
-    fmt.Println("汪汪")
+    fmt.Printf("%s:汪汪\n",d.name)
 }
 
 func main(){
     d1 := newDog("dog01")
+    d2 := newDog("dog02")
     d1.wang()
+    d2.wang()
 }
